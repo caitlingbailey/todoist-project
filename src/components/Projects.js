7697a51c1691f833a89e115e539bef70819c67db
caitlingbailey <64934329+caitlingbailey@users.noreply.github.com>
2020-06-22 11:15:13 +0100

    Sidebar styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,31 +9,65 @@ export const Projects = ({ activeValue = null}) => {
     const {projects} = useProjectsValue();
 
 
-    return (
-        projects && 
-        projects.map(project => (
-            <li
-                key={project.projectid}
-                data-doc-id={project.docid}
-                data-testid="project-action"
-                type="button"
-                className={
-                    active === project.projectid
-                    ? 'active sidebar__project'
-                    : 'sidebar_project'
-                }
-                onKeyDown={() => {
-                    setActive(project.projectid);
-                    setSelectedProject(project.projectid);
-                }}
-                onClick={() => {
-                    setActive(project.projectid);
-                    setSelectedProject(project.projectid);
-                }}
-            >
-                <IndividualProject project={project} />
-            </li>
+    // return (
+    //     projects && 
+    //     projects.map(project => (
+    //         <li
+    //             key={project.projectid}
+    //             data-doc-id={project.docid}
+    //             data-testid="project-action"
+    //             type="button"
+    //             className={
+    //                 active === project.projectid
+    //                 ? 'active sidebar__project'
+    //                 : 'sidebar_project'
+    //             }
+    //             onKeyDown={() => {
+    //                 setActive(project.projectid);
+    //                 setSelectedProject(project.projectid);
+    //             }}
+    //             onClick={() => {
+    //                 setActive(project.projectid);
+    //                 setSelectedProject(project.projectid);
+    //             }}
+    //         >
+    //             <IndividualProject project={project} />
+    //         </li>
 
+    //     ))
+    // );
+    return (
+        projects &&
+        projects.map((project) => (
+          <li
+            key={project.projectid}
+            data-testid="project-action-parent"
+            data-doc-id={project.docid}
+            className={
+              active === project.projectid
+                ? 'active sidebar__project'
+                : 'sidebar__project'
+            }
+          >
+            <div
+              role="button"
+              data-testid="project-action"
+              tabIndex={0}
+              aria-label={`Select ${project.name} as the task project`}
+              onClick={() => {
+                setActive(project.projectid);
+                setSelectedProject(project.projectid);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  setActive(project.projectid);
+                  setSelectedProject(project.projectid);
+                }
+              }}
+            >
+              <IndividualProject project={project} />
+            </div>
+          </li>
         ))
-    );
+      );
 };
